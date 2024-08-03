ff6c5d780048b9bf318a0d12d639df64ba27b59e
originalTOXIC <146562948+originalTOXIC@users.noreply.github.com>
2024-08-03 03:18:18 +0100

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     var popup = document.getElementById("valorant-popup");
     var link = document.getElementById("valorant-link");
-    var close = document.getElementsByClassName("close")[0];
 
     link.onclick = function(event) {
         event.preventDefault();
         popup.style.display = "block";
-    }
-
-    close.onclick = function() {
-        popup.style.display = "none";
     }
 
     window.onclick = function(event) {
