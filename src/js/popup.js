0c8e4af75feef1aa7d3d5584adbe9979bb80d231
originalTOXIC <146562948+originalTOXIC@users.noreply.github.com>
2024-08-06 04:58:00 +0100

    Update popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var popup = document.getElementById("valorant-popup");
-    var link = document.getElementById("valorant-link");
+    var valorantPopup = document.getElementById("valorant-popup");
+    var valorantLink = document.getElementById("valorant-link");
+    var discordPopup = document.getElementById("discord-popup");
+    var discordLink = document.getElementById("discord-link");
 
-    link.onclick = function(event) {
+    valorantLink.onclick = function(event) {
         event.preventDefault();
-        popup.style.display = "block";
+        valorantPopup.style.display = "block";
+    }
+
+    discordLink.onclick = function(event) {
+        event.preventDefault();
+        discordPopup.style.display = "block";
     }
 
     window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.style.display = "none";
+        if (event.target == valorantPopup) {
+            valorantPopup.style.display = "none";
+        }
+        if (event.target == discordPopup) {
+            discordPopup.style.display = "none";
         }
     }
 });
